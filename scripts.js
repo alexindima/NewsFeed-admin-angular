6d9f6d7747d314f6e3b828769d7d9ee249871710
AlexinD <dalexin@codemastersintl.com>
2022-10-13 02:37:39 +0300

    Some more actions

--- scripts.js
+++ scripts.js
@@ -14,12 +14,12 @@ arrowBottom = document.getElementById("arrowBottom")
 document.getElementById("user").addEventListener('click', function () {
 
         document.querySelector('.modal_area').style.display = 'flex';
-        document.querySelector('.wrapper').classList.add('modal-opened');
+        document.querySelector('.body-general').classList.add('modal-opened');
 });
 
 document.getElementById("modal_close").addEventListener('click', function () {
         document.querySelector('.modal_area').style.display = 'none';
-        document.querySelector('.wrapper').classList.remove('modal-opened');
+        document.querySelector('.body-general').classList.remove('modal-opened');
 });
 
 document.getElementById("search").addEventListener('click', function () {
@@ -41,39 +41,84 @@ document.getElementById("dropdown").addEventListener('click', function () {
 });
 
 /* Отслеживание скролла для изменения высоты хедера и появления поля возврата к верху страницы */
-window.addEventListener('scroll', function() {
-    document.querySelector('.scroll-button').style.top = document.querySelector('.header').style.height + "px";
-    arrowTop.hidden = (pageYOffset < upThreshold);
-    if (pageYOffset > forgetThreshold) {
-        scrollWas = false;
+
+/* Вынес для устранения повтора */
+function SetBigHeader() {
+    document.querySelector('.header').classList.remove('header_small');
+    document.querySelector('.logo_img').classList.remove('logo_img_small');
+}
+
+function SetSmallHeader() {
+    document.querySelector('.header').classList.add('header_small');
+    document.querySelector('.logo_img').classList.add('logo_img_small');
+}
+
+function CalculateHeader() {
+    if (scrollY  > downThreshold) {
+        SetSmallHeader();
     }
-    if ((scrollWas == true) && (pageYOffset < bigHeader)) {
-        arrowBottom.hidden = false;
-    }
-    else {
-        arrowBottom.hidden = true;
-    }
-    if (pageYOffset > downThreshold) {
-        document.querySelector('.header').classList.add('header_small');
-        document.querySelector('.logo_img').classList.add('logo_img_small');
-    }
-    else {
-        if (pageYOffset < bigHeader) {
-            document.querySelector('.header').classList.remove('header_small');
-            document.querySelector('.logo_img').classList.remove('logo_img_small');
+    else { /* Гистерезис */
+        if (scrollY  < bigHeader) {
+            SetBigHeader();
         }
     }
-});
+}
+
+function CalculateScroll() {
+    arrowTop.hidden = (scrollY  < upThreshold);
+    if (scrollY  > forgetThreshold) {
+        scrollWas = false;
+    }
+    arrowBottom.hidden = !((scrollWas) && (scrollY < bigHeader));
+}
+
+function CalculateScrollAndHeader() {
+    let scrollWidth = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.body.clientWidth, document.documentElement.clientWidth
+    );
+    if (scrollWidth > 768) {
+        CalculateScroll();
+        CalculateHeader();
+    }
+    else {
+        SetSmallHeader();
+    }
+}
+
+/* Считаем хедер и скролл-ап при скролле страницы и если ширина просмотра больше чем 768 */
+window.addEventListener('scroll', function() {
+    let scrollWidth = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.body.clientWidth, document.documentElement.clientWidth
+    );
+    if (scrollWidth > 768) {
+        CalculateScroll();
+        CalculateHeader();
+    }
+})
+
+/* Считаем хедер и скролл-ап при ресайзе браузера */
+window.addEventListener('resize', function() {
+    CalculateScrollAndHeader();
+})
+
+/* Считаем хедер и скролл-ап при загрузке страницы для маленького хедера при скукоженном экране */
+window.addEventListener('DOMContentLoaded', function() {
+    CalculateScrollAndHeader();
+})
 
 /* Две функции - клик на поле возврата к верху страницы / возврату к предыдущему месту */
 arrowTop.addEventListener('click', function() {
     scrollWas = true
-    lastPoint = pageYOffset;
-    window.scrollTo(pageXOffset, 0);
+    lastPoint = scrollY ;
+    window.scrollTo(scrollX , 0);
     arrowBottom.hidden = false;
     // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
-});
+})
 
 arrowBottom.addEventListener('click', function () {
-    window.scrollTo(pageXOffset, lastPoint);
-});
+    window.scrollTo(scrollX , lastPoint);
+})
