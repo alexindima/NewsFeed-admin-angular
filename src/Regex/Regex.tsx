ebc9c85ddd2971dd504bbbc263ff755505e59954
Roman Borisov <rborisov@codemastersintl.com>
2022-12-18 23:38:58 +0400

    * added feedback

--- src/Regex/Regex.tsx
+++ src/Regex/Regex.tsx
@@ -1,2 +1,3 @@
-export const validUserName = new RegExp('^[a-zA-Z0-9_]{3,}$');
+// Некорректное название файла. Этот файл содержит правила валидации, можешь назвать что-то типа custom valid rules
+export const validUserName = new RegExp('^[a-zA-Z0-9_]{3,}$'); // IDE подсказывает "'[a-zA-Z0-9_]' can be simplified to '\w' "
 export const validPassword = new RegExp('^[a-zA-Z0-9:;!#$%^&*()_=+]{6,}$');
